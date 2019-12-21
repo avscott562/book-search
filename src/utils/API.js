0fc5b844aa00ca367cdbc8dad6f65a4251095dd1
Scottie <avscott@pacbell.net>
2019-12-21 04:24:49 -0800

    add get request for api

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,5 +1,9 @@
-import Axios from 'axios';
+require('dotenv').config();
+import axios from 'axios';
 
 export default {
     //get requets go here
+    findBooks: function(search) {
+        return axios.get(`https://www.googleapis.com/books/v1/volumes?q=${search}&key=${process.env.API_KEY}`);
+    }
 }
