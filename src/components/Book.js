c1bd4601ea100ecb6f13e267e510afbd52b4f511
Scottie <avscott@pacbell.net>
2019-12-21 02:01:21 -0800

    update book component styling

--- src/components/Book.js
+++ src/components/Book.js
@@ -4,26 +4,29 @@ class Book extends Component {
     state = {
         title: "Test Title",
         author: "Tester McTester",
-        summary: "Summary of the book test title.",
-        image: "https://via.placeholder.com/150"
+        summary: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vel quam elementum pulvinar etiam non. Elementum sagittis vitae et leo. Neque laoreet suspendisse interdum consectetur libero. Id diam maecenas ultricies mi eget mauris pharetra et. Mattis enim ut tellus elementum sagittis vitae et. Fermentum iaculis eu non diam phasellus vestibulum lorem sed. Vulputate mi sit amet mauris commodo. Tristique risus nec feugiat in fermentum. Interdum posuere lorem ipsum dolor sit. Montes nascetur ridiculus mus mauris vitae. Ornare arcu odio ut sem nulla pharetra diam sit amet. Et ultrices neque ornare aenean euismod elementum nisi quis. Et malesuada fames ac turpis egestas maecenas pharetra. Mus mauris vitae ultricies leo integer malesuada nunc vel risus. Cursus sit amet dictum sit amet justo. Ut placerat orci nulla pellentesque dignissim. Vitae tempus quam pellentesque nec nam aliquam sem et. Ultricies lacus sed turpis tincidunt id aliquet risus feugiat in. Maecenas ultricies mi eget mauris pharetra et ultrices neque ornare.",
+        image: "https://via.placeholder.com/200"
     }
 
     render() {
         return (
             <div>
                 <div className="row">
-                    <div className="col-9">
+                    <div className="col-10">
                         <p>{this.state.title}</p>
                         <p>{this.state.author}</p>
                     </div>
-                    <div className="col-3">
+                    <div className="col-2 float-right">
                         <button className = "btn btn-outline-secondary btn-sm">View</button>
                         <button className = "btn btn-outline-secondary btn-sm ml-2">Save</button>
                     </div>
                 </div>
-                
-                <p><img src={this.state.image} className = "float-left"></img>
-                <span className = "pl-3">{this.state.summary}</span></p>
+                <div className = "row">
+                    <div className = "col-12">
+                        <img src={this.state.image} className = "float-left pr-4"></img>
+                        <div className = "pr-2">{this.state.summary}</div>
+                    </div>
+                </div>
             </div>
         )
     }
