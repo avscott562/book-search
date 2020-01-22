f595c8aa161a1052b53695bd9fb7280c5e8e2aa1
Scottie <avscott@pacbell.net>
2020-01-21 22:43:33 -0800

    update api utils

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,9 +1,23 @@
-require('dotenv').config();
 import axios from 'axios';
 
 export default {
-    //get requets go here
+    // search for books
     findBooks: function(search) {
         return axios.get(`https://www.googleapis.com/books/v1/volumes?q=${search}&key=${process.env.API_KEY}`);
+    },
+
+    // get all books from database
+    getBooks: function() {
+        return axios.get('/api/books');
+    },
+
+    // add new book to database
+    addBook: function(bookData) {
+        return axios.post('/api/books', bookData);
+    },
+
+    // delete book from database
+    deleteBook: function(id) {
+        return axios.delete("/api/books" + id);
     }
-}
+};
